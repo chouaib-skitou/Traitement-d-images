674a73c88657e147ede734b746583ff6f42b3bf2
HamCam203 <camarahamoudia@gmail.com>
2024-09-06 12:20:53 +0200

    fonction de rotation, correction recherche automatique angle

--- tp1/rotation.py
+++ tp1/rotation.py
@@ -19,15 +19,17 @@ def analyser_fichier(fichier, index_recherche):
             
             # Vérifier si l'index correspond à celui recherché
             if index == index_recherche:
+                print(angle)
                 return angle
 
 
-num = 24
+
+num = 4
 image = cv2.imread("frag_eroded/frag_eroded_" + str(num) + ".png")
 
 (h, w) = image.shape[:2] # on récupère la hauteur et la largeur
 center = (w / 2, h / 2) # on en déduit la position du centre de l'image
-angle = analyser_fichier("fragments.txt",24) #-88.581 # on indique l'angle de rotation voulue
+angle = analyser_fichier("fragments.txt",num) #-88.581 # on indique l'angle de rotation voulue
 scale = 1 # on précise l'échelle de l'image
 
 M = cv2.getRotationMatrix2D(center, angle, scale)
